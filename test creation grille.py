e8d73712d42aa043e1651c20693326fe992ca0fb
Coline VOLIVERT <coline.volivert@ens.usvq.fr>
2022-02-20 00:05:52 +0100

    avancement sur la fct

--- test creation grille.py
+++ test creation grille.py
@@ -1,12 +1,48 @@
+import random as rd
+import tkinter as tk
+#CONSTANTES
+
+#taille de la grille
+L = 4
+#hauteur de la grille
+HEIGHT = 500
+#largeur de la grille
+WIDTH = 500
+
+#variable globale
 grille = []
-for i in range(L):
-    for j in range(L):
-            grille.append([i,j])
-print(grille)
 
 
-for i in range(L):
-    for j in range(L):
-        nb = rd.randint(0,3)
-        case = tk.Label(racine, text=str(nb))
-        case.grid(column=i, row=j)
+####################
+# fonctions
+
+def initialisation():
+    """initialisation du terrain :
+    * creation de la grille avec toutes les cases à 0"""
+    global grille
+    for i in range(L):
+        grille.append([0]*L)
+
+    for i in range(L):
+        for j in range(L):
+            nb = rd.randint(0,3)
+            case = canvas.create_text((i,j), text=str(nb))
+            grille[i][j] = case
+
+
+# partie pricipale
+
+# création widgets
+racine = tk.Tk() # Création de la fenêtre racine
+bouton = tk.Button(racine, text="initialisation", command=initialisation)
+canvas = tk.Canvas(racine, height=HEIGHT, width=WIDTH)
+canvas.create_text(100,500, text="hello")
+
+#placement des widgets
+bouton.grid(column=0, row=1)
+canvas.grid(column=0, row=0)
+
+#initialisation()
+
+#boucle principale
+racine.mainloop()
