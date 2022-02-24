a55429f1128f0ef1b3324954a8d20344d9df4e26
Coline VOLIVERT <coline.volivert@ens.usvq.fr>
2022-02-24 10:40:02 +0100

    creation nouvelle fonction

--- test creation grille.py
+++ test creation grille.py
@@ -10,8 +10,10 @@ HEIGHT = 500
 WIDTH = 500
 
 #variable globale
-grille = []
-
+grille = [] #contient uniquement les id des cases pour modif param. graphiques
+"""Q : créé une autre liste qui stock le nb de grains de sable ?
+(oui si operation sur valeur necessaire)
+[sinon obligé de recuperer valeur dans str du rectangle = galere ?]"""
 
 ####################
 # fonctions
@@ -25,7 +27,16 @@ def initialisation():
 
     for i in range(L):
         for j in range(L):
-            nb = rd.randint(0,3)
+            case = canvas.create_text((i,j), text=0) #tsr oblig dans text= ??
+            grille[i][j] = case
+
+
+def insert_valeur():
+    """insertion des valeurs dans la grille"""
+    global grille
+    for i in range(L):
+        for j in range(L):
+            nb = rd.randint(0,3) #utiliser rd.uniform ?
             case = canvas.create_text((i,j), text=str(nb))
             grille[i][j] = case
 
@@ -36,7 +47,6 @@ def initialisation():
 racine = tk.Tk() # Création de la fenêtre racine
 bouton = tk.Button(racine, text="initialisation", command=initialisation)
 canvas = tk.Canvas(racine, height=HEIGHT, width=WIDTH)
-canvas.create_text(100,500, text="hello")
 
 #placement des widgets
 bouton.grid(column=0, row=1)
