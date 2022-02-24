664aee9eee9e87f628ecf2ccd99d1df98672e537
Coline VOLIVERT <coline.volivert@ens.usvq.fr>
2022-02-24 10:52:20 +0100

    modif fct

--- test creation grille.py
+++ test creation grille.py
@@ -5,15 +5,16 @@ import tkinter as tk
 #taille de la grille
 L = 4
 #hauteur de la grille
-HEIGHT = 500
+HAUTEUR = 500
 #largeur de la grille
-WIDTH = 500
+LARGEUR = 500
 
 #variable globale
 grille = [] #contient uniquement les id des cases pour modif param. graphiques
 """Q : créer une autre liste qui stock le nb de grains de sable ?
 (oui si operation sur valeur necessaire)
 [sinon obligé de recuperer valeur dans str du rectangle = galere ?]"""
+l_case = LARGEUR//L
 
 ####################
 # fonctions
@@ -27,17 +28,17 @@ def initialisation():
 
     for i in range(L):
         for j in range(L):
-            case = canvas.create_text((i,j), text=0) #tsr oblig dans text= ??
-            grille[i][j] = case
+            case = canvas.create_text((i*l_case,j*l_case), text=0)
+            grille[i][j] = case #recuperation id pour modif +tard
 
 
 def insert_valeur():
-    """insertion des valeurs dans la grille"""
+    """insertion des valeurs dans la grille à la place des 0"""
     global grille
     for i in range(L):
         for j in range(L):
             nb = rd.randint(0,3) #utiliser rd.uniform ?
-            case = canvas.create_text((i,j), text=str(nb))
+            case = canvas.create_text((i*l_case,j*l_case), text=str(nb))
             grille[i][j] = case
 
 
@@ -46,7 +47,7 @@ def insert_valeur():
 # création widgets
 racine = tk.Tk() # Création de la fenêtre racine
 bouton = tk.Button(racine, text="initialisation", command=initialisation)
-canvas = tk.Canvas(racine, height=HEIGHT, width=WIDTH)
+canvas = tk.Canvas(racine, height=HAUTEUR, width=LARGEUR)
 
 #placement des widgets
 bouton.grid(column=0, row=1)
