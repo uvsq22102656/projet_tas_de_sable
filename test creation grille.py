9e6631566470a3ca775b6e54e412c06db2da1e85
Coline VOLIVERT <coline.volivert@ens.usvq.fr>
2022-03-29 22:30:39 +0200

    tentative creation fct stabilisation

--- test creation grille.py
+++ test creation grille.py
@@ -16,6 +16,7 @@ grille = [] #contient uniquement les id des cases pour modif param. graphiques
 [sinon obligé de recuperer valeur dans str du rectangle = galere ?]"""
 l_case = LARGEUR//L
 nb_clics = 0
+nb = 0
 
 ####################
 # fonctions
@@ -23,7 +24,7 @@ nb_clics = 0
 def initialisation():
     """initialisation du terrain :
     - creation de la grille avec toutes les cases à 0"""
-    global grille,nb_clics
+    global grille, nb_clics, nb
     if nb_clics == 0:
         for i in range(L):
             grille.append([0]*L)
@@ -36,21 +37,53 @@ def initialisation():
         nb_clics = 1
 
     else:
-        insert_valeur()
+        insert_valeur(3) #configuration stable
 
 
-def insert_valeur():
-    """insertion des valeurs dans la grille à la place des 0"""
-    global grille
+def insert_valeur(n):
+    """insertion des valeurs comprises entre 0 et n dans la grille à la place des 0"""
+    global grille, case,
     canvas.delete("all")
-    liste_coul = ['lightgoldenrod','gold', 'goldenrod','darkgoldenrod']
+    liste_coul = ['lightgoldenrod','gold', 'goldenrod','darkgoldenrod','brown']
     for i in range(L):
         for j in range(L):
-            nb = rd.randint(0,3)
+            nb = rd.randint(0,n)
+            if nb>3: nb=4
             rect = canvas.create_rectangle((i*l_case,j*l_case), ((i+1)*l_case,(j+1)*l_case),
                         fill=liste_coul[nb])
             case = canvas.create_text((i*l_case+l_case//2,j*l_case+l_case//2), text=str(nb))
             grille[i][j] = case
+
+def stabilisation():
+    """focntion qui ne code uniquement la stabilisation (avalanche)
+    pour ensuite creer une focntion qui ne code uniquement l'affichage sur la canvas de la configuration
+    (Q: ne modifier que la grille et la renvoyer a la fin de la fonction ?)"""
+    global grille, case, nb
+    for i in range(L):
+        for j in range(L):
+            if canvas.itemconfigure('text')>3:
+                nb-=1
+                canvas.itemconfigure(grille[i][j], text=str(nb))
+                canvas.itemconfigure(grille[i][j], text=str(nb)) #tentative de modifier nb des autres case lors de l'avalanche
+    
+
+def config_rd():
+    """- creer une configuration aléatoire sur le canvas
+    - se stabilise ensuite"""
+    global grille, case, nb
+    canvas.delete("all")
+    liste_coul = ['lightgoldenrod','gold', 'goldenrod','darkgoldenrod']
+    for i in range(L):
+        for j in range(L):
+            nb = rd.randint(0,5)
+            rect = canvas.create_rectangle((i*l_case,j*l_case), ((i+1)*l_case,(j+1)*l_case),
+                        fill=liste_coul[nb])
+            case = canvas.create_text((i*l_case+l_case//2,j*l_case+l_case//2), text=str(nb))
+            grille[i][j] = case
+    canvas.after(2000, stabilisation)
+
+
+
 
 
 
@@ -60,12 +93,14 @@ def insert_valeur():
 racine = tk.Tk() # Création de la fenêtre racine
 bouton = tk.Button(racine, text="initialisation", command=initialisation)
 canvas = tk.Canvas(racine, height=HAUTEUR, width=LARGEUR, bd=10)
+bouton_config = tk.Button(racine, text="configuration aléatoire", command=config_rd)
 
 
 
 #placement des widgets
 bouton.grid(column=0, row=1)
-canvas.grid(column=0, row=0)
+canvas.grid(column=0, row=0, columnspan=2)
+bouton_config.grid(column=1, row=1)
 
 
 #boucle principale
